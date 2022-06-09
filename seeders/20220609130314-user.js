37239048108e43f2de0ab72fd4f792efd4d12f3f
ahsanul-hub <aldi.madridista.am@gmail.com>
2022-06-10 02:41:45 +0700

    commit on 'update seed'

--- seeders/20220609130314-user.js
+++ seeders/20220609130314-user.js
@@ -16,7 +16,8 @@ module.exports = {
       [
         {
           email: "admin@mail.com",
-          password: "asdasd", //123456
+          password:
+            "$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm", //123456
           name: "admin",
           status: "admin",
           createdAt: new Date(),
