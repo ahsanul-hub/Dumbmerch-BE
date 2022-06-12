beb6e6ed67cb9fe8599608c162e17265f9ee8579
ahsanul-hub <aldi.madridista.am@gmail.com>
2022-06-12 18:39:26 +0700

    update cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -8,29 +8,29 @@ exports.addCart = async (req, res) => {
       qty: 1,
     };
 
-    const productExist = await cart.findOne({
-      where: {
-        idProduct: parseInt(req.body.idProduct),
-        idBuyer: req.user.id,
-      },
-      attributes: {
-        exclude: ["createdAt", "updatedAt"],
-      },
-    });
+    // const productExist = await cart.findOne({
+    //   where: {
+    //     idProduct: parseInt(req.body.idProduct),
+    //     idBuyer: req.user.id,
+    //   },
+    //   attributes: {
+    //     exclude: ["createdAt", "updatedAt"],
+    //   },
+    // });
 
-    if (productExist) {
-      const addQty = {
-        qty: productExist.qty + 1,
-      };
+    // if (productExist) {
+    //   const addQty = {
+    //     qty: productExist.qty + 1,
+    //   };
 
-      await cart.update(addQty, {
-        where: {
-          id: productExist.id,
-        },
-      });
-    } else {
-      await cart.create(data);
-    }
+    //   await cart.update(addQty, {
+    //     where: {
+    //       id: productExist.id,
+    //     },
+    //   });
+    // } else {
+    // }
+    await cart.create(data);
 
     res.send({
       status: "success",
